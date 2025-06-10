98d186597253fc017a3d1cc88726a90dd4e4ae5d
JGabZXC <jlopezgab25@gmail.com>
2025-06-10 15:35:52 +0800

    caching public

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`, { maxAge: "30d" }));
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
